739ab9eceb30d2db7481ca5c43903427e0c35580
Vivek <vivek.chetia@clinifyhealth.com>
2022-04-19 17:44:32 +0530

    beta: updated

--- router/book/index.ts
+++ router/book/index.ts
@@ -2,9 +2,8 @@ import { Router } from "express";
 import * as BookController from "../../controller/book";;
 
 const router = Router();
-
+// book routes
 router.get('/all', BookController.getAllBooks);
-
 router.get('/details/:id', BookController.getBooksDetailsByBookId);
 
 export default router;
