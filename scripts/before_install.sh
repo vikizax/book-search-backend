e9122686f43b0e64ae8dd4fe0fa5fe1e028e4270
Vivek <vivek.chetia@clinifyhealth.com>
2022-04-19 17:34:04 +0530

    beta: updated

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #dowload node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 14.7.0
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+# . ~/.nvm/nvm.sh
+# nvm install 14.7.0
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/book-search/backend"
